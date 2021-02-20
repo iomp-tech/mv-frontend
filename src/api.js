5afce9666aedd9f3609282b21ef25a9e757a837b
Арсений Нагибин <58362217+nagibin88@users.noreply.github.com>
2021-02-20 14:47:26 +0300

    fix bugs

--- src/api.js
+++ src/api.js
@@ -2,6 +2,6 @@ export const API_DOMEN = "https://api.iomp.ru/public/api";
 
 export const DOMEN = "https://api.iomp.ru";
 //http://127.0.0.1:8000/api
-//http://api.iomp.ru/public/api
+//https://api.iomp.ru/public/api
 
 export const CART_DOMEN = "https://shop.iomp.ru/?r=ordering/cart/s1&lg=ru";
