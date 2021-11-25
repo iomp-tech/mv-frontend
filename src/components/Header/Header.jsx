57f75ab96008af46d68f1f23b5ca78ae52245601
nagibinstudio <nagibinstudio@gmail.com>
2021-11-25 23:38:12 +0300

    25.11.2021

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,6 +17,7 @@ const Header = React.memo(() => {
 
     const [visibleButtonCart, setVisibleButtonCart] = React.useState(false);
 
+    const cartItems = useSelector(({cart}) => cart.cart);
     const {user, isLoaded, isLogin} = useSelector(({user}) => user);
 
     React.useEffect(() => {
@@ -42,8 +43,8 @@ const Header = React.memo(() => {
                 }`}
             >
                 <svg
-                    width="25"
-                    height="25"
+                    width="28"
+                    height="28"
                     viewBox="0 0 39 40"
                     xmlns="http://www.w3.org/2000/svg"
                 >
@@ -60,9 +61,13 @@ const Header = React.memo(() => {
                         d="M32.1956 32.9766C30.4846 32.9766 29.1018 34.3516 29.1018 36.0547C29.1018 37.7578 30.4846 39.1406 32.1956 39.1406C33.9065 39.1406 35.3753 37.7656 35.2893 36.0625C35.2893 34.3594 33.9065 32.9766 32.1956 32.9766ZM33.6643 36.0625C33.6643 36.875 33.0159 37.5234 32.1956 37.5234C31.3752 37.5234 30.7268 36.875 30.7268 36.0625C30.7268 35.25 31.3752 34.6016 32.1956 34.6016C33.0159 34.6016 33.7424 35.25 33.6643 36.0625Z"
                     />
                 </svg>
+
+                <span className="header-cart-button__number">
+                    {Object.keys(cartItems).length}
+                </span>
             </Link>
 
-            <ScrollToTop
+            {/* <ScrollToTop
                 showUnder={500}
                 duration={1000}
                 style={{zIndex: "1000"}}
@@ -99,7 +104,8 @@ const Header = React.memo(() => {
                         />
                     </svg>
                 </div>
-            </ScrollToTop>
+			</ScrollToTop> */}
+
             <header className="header">
                 <div className="container">
                     <HeaderTop />
